ba566871563a23846eb880d75e6f7c05284a412a
Antor Paul <antorpaul@yahoo.com>
2023-06-27 18:00:31 -0700

    19. Pushed initial attempt that doesn't work for all lengths

--- RemoveNthNodeFromEndOfList/Solution.py
+++ RemoveNthNodeFromEndOfList/Solution.py
@@ -1,4 +1,3 @@
-from CommonPy import LinkedList
 import typing
 
 # Definition for singly-linked list.
@@ -15,18 +14,55 @@ class ListNode:
     def createList(array: list):
         if len(array) >= 1:
             head: ListNode = None
-            currPtr = None
+            current = None
             for i in range(0, len(array)):
                 newNode = ListNode(array[i])
                 if not head:
                     head = newNode
-                    currPtr = head
+                    current = head
                 else:
-                    currPtr.next = newNode
-                    currPtr = currPtr.next
+                    current.next = newNode
+                    current = current.next
             return head
         return None
 
+    def addNode(self, val: int):
+        """ Adds a node to LinkedList """
+        current = self
+        newNode: ListNode = ListNode(val, None)
+        if current:
+            while current != None:
+                if current.next == None:
+                    current.next = newNode
+                    current = current.next
+                current = current.next
+        else:
+            head = newNode
+
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        pass
+        current: ListNode = head
+        previous: ListNode = None
+        remove: ListNode = None
+        trail: int = 0
+        i: int = 0
+
+        while current:
+            previous = remove
+            if i - n == 0:
+                remove = current
+                trail = 0
+            if i > n and trail == n:
+                remove = remove.next
+                trail = 0
+
+            i += 1
+            trail += 1
+            current = current.next
+
+        if remove and remove.next:
+            previous.next = remove.next
+        elif remove and previous:
+            previous.next = None
+        else:
+            head = None
--- RemoveNthNodeFromEndOfList/test_Solution.py
+++ RemoveNthNodeFromEndOfList/test_Solution.py
@@ -1,4 +1,5 @@
 import unittest
+import typing
 from Solution import Solution, ListNode
 
 class test_Solution(unittest.TestCase):
@@ -6,7 +7,7 @@ class test_Solution(unittest.TestCase):
         self.sol = Solution()
         return super().setUp()
 
-    def compareLists(self, given: ListNode, expected: List):
+    def compareLists(self, given: ListNode, expected: list):
         """ Compares a linked list to an expected list """
         currNode: ListNode = given
         i: int = 0
@@ -24,5 +25,56 @@ class test_Solution(unittest.TestCase):
         # Check length
         self.assertEqual(i, len(expected))
 
+    def comparedLinkedLists(self, given: ListNode, expected: ListNode):
+        """ Compares a linked list to another linked list """
+        currentGiven = given
+        currentExpected = expected
+
+        if currentGiven and currentExpected:
+            while currentGiven != None:
+                if currentExpected:
+                    self.assertEqual(currentGiven.val, currentExpected.val)
+                else:
+                    raise IndexError("Given list has more values than expected.")
+                currentGiven = currentGiven.next
+                currentExpected = currentExpected.next
+
+        if currentExpected:
+            raise IndexError("Given list does not have all expected nodes.")
+
+    # Linked List Tests
+    def test_Append(self):
+        head: ListNode = ListNode.createList([1,2,3,4,5])
+        head.addNode(6)
+        self.comparedLinkedLists(head, ListNode.createList([1,2,3,4,5,6]))
+
+    # Problem Tests
     def test_CaseOne(self):
-        head: ListNode = ListNode.
+        # given
+        head: ListNode = ListNode.createList([1,2,3,4,5])
+        expected: list[int] = [1,2,3,5]
+        
+        # then
+        self.sol.removeNthFromEnd(head, 2)
+
+        self.compareLists(head, expected)
+
+    def test_CaseTwo(self):
+        # given
+        head: ListNode = ListNode.createList([1])
+        expected: list[int] = []
+
+        # then
+        self.sol.removeNthFromEnd(head, 1)
+
+        self.compareLists(head, expected)
+
+    def test_CaseThree(self):
+        # given
+        head: ListNode = ListNode.createList([1,2])
+        expected: list[int] = [1]
+
+        # then
+        self.sol.removeNthFromEnd(head, 1)
+
+        self.compareLists(head, expected)
