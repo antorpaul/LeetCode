6f1bfb99ba38effe557633b3bc3ca333c9e796ad
Antor Paul <antorpaul@yahoo.com>
2023-07-09 20:43:30 -0700

    138 Copy List With Random Pointer
    Initial set up for problem.

---
+++ CopyListWithRandomPointer/test_Solution.py
@@ -0,0 +1,49 @@
+import unittest
+import typing
+from Solution import Solution, ListNode
+
+class test_Solution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = Solution()
+        return super().setUp()
+
+    def compareLists(self, given: ListNode, expected: list):
+        """ Compares a linked list to an expected list """
+        currNode: ListNode = given
+        i: int = 0
+
+        # Check for empty
+        if len(expected) < 1:
+            self.assertEqual(currNode, None)
+
+        # Check value of each node against expected array
+        while currNode != None:
+            self.assertEqual(currNode.val, expected[i])
+            i += 1
+            currNode = currNode.next
+
+        # Check length
+        self.assertEqual(i, len(expected))
+
+    def comparedLinkedLists(self, given: ListNode, expected: ListNode):
+        """ Compares a linked list to another linked list """
+        currentGiven = given
+        currentExpected = expected
+
+        if currentGiven and currentExpected:
+            while currentGiven != None:
+                if currentExpected:
+                    self.assertEqual(currentGiven.val, currentExpected.val)
+                else:
+                    raise IndexError("Given list has more values than expected.")
+                currentGiven = currentGiven.next
+                currentExpected = currentExpected.next
+
+        if currentExpected:
+            raise IndexError("Given list does not have all expected nodes.")
+
+    # Linked List Tests
+    def test_Append(self):
+        head: ListNode = ListNode.createList([1,2,3,4,5])
+        head.addNode(6)
+        self.comparedLinkedLists(head, ListNode.createList([1,2,3,4,5,6]))
