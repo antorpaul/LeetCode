f06c53cfc77528f0d89b36478d50f4beb17c9faf
Antor Paul <antorpaul@yahoo.com>
2023-07-14 15:03:33 -0700

    226 Invert Binary Tree
    Swap left and right nodes throughout the tree recursively

---
+++ InvertBinaryTree/test_Solution.py
@@ -0,0 +1,11 @@
+import unittest
+import Solution
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = Solution.Solution()
+
+        return super().setUp()
+    
+    def caseOne(self):
+        pass
